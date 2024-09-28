f9ff01dd060b4b8edca5347e1e2f6b0cb8512884
1dominik7 <dominik-wicek@o2.pl>
2024-09-28 10:54:49 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   public/amazon1.png
    	new file:   public/amazon2.png
    	new file:   public/amazon3.png
    	new file:   public/amazon4.png
    	new file:   public/chair1.png
    	new file:   public/chair2.png
    	new file:   public/chair3.png
    	new file:   public/chair4.png
    	new file:   public/meow1.png
    	new file:   public/meow2.png
    	new file:   public/meow3.png
    	new file:   public/meow4.png
    	new file:   public/podify1.png
    	new file:   public/podify2.png
    	new file:   public/podify3.png
    	new file:   public/podify4.png
    	new file:   public/realestate1.png
    	new file:   public/realestate2.png
    	new file:   public/realestate3.png
    	new file:   public/realestate4.png
    	modified:   src/app.scss
    	modified:   src/components/contact/contact.scss
    	modified:   src/components/hero/hero.scss
    	modified:   src/components/navbar/navbar..scss
    	modified:   src/components/parallax/parallax.scss
    	modified:   src/components/portfolio/Portfolio.jsx
    	modified:   src/components/portfolio/portfolio.scss

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,12 +1,16 @@
 import { useRef } from "react";
 import "./portfolio.scss";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import { Swiper, SwiperSlide } from "swiper/react"; 
+import { Navigation } from "swiper/modules";
+import "swiper/css";                // Core Swiper styles
+import "swiper/css/navigation";  
 
 const items = [
   {
     id: 1,
     title: "MeowCase-Store",
-    img: "./meow-Case.png",
+    images: ["./meow1.png", "./meow2.png", "./meow3.png", "./meow4.png"],
     desc: "MeowCase is an e-commerce platform developed using Next.js with TypeScript, leveraging the Kind library for user authentication and Neon for database management (a PostgreSQL platform). The app allows users to design and create custom cases for their iPhones, featuring the ability to upload images and personalize their cases. MeowCase provides a seamless shopping experience with secure authentication, fast performance, and an intuitive interface that makes it easy for users to create, preview, and order their custom cases directly from the app.",
     tools: [
       "./nextjs.png",
@@ -19,7 +23,12 @@ const items = [
   {
     id: 2,
     title: "RealEstate Website",
-    img: "./realestate.png",
+    images: [
+      "./realestate1.png",
+      "./realestate2.png",
+      "./realestate3.png",
+      "./realestate4.png",
+    ],
     desc: "The real estate app is built using React.js for the frontend and Node.js on the backend, with MongoDB as the database, managed through Prisma. It allows users to buy, sell, or rent houses and apartments. Users can browse listings, post their own offers, and communicate directly with other users or professional real estate agents through a built-in messaging system. The app is optimized for fast, seamless interactions and provides an intuitive platform for real-time communication, whether you're searching for a home or managing your listings.",
     tools: [
       "./reactIcon.png",
@@ -33,7 +42,7 @@ const items = [
   {
     id: 3,
     title: "AkRacing-demo",
-    img: "./chair-Store.png",
+    images: ["./chair1.png", "./chair2.png", "./chair3.png", "./chair4.png"],
     desc: "AkRacing-DemoPage is an e-commerce platform built with Vue.js on the frontend, Node.js on the backend, and MongoDB for the database. It replicates the AKRacing store, offering a seamless experience for purchasing professional gaming and office chairs. The platform includes an admin panel that allows administrators to manage the store effectively, with features to add new products, edit existing ones, and oversee inventory. Users can easily browse chair models, view detailed product specs, and securely complete purchases. The admin panel ensures that product management and updates are simple and efficient.",
     tools: ["./vueLogo.png", "./nodeIcon.png", "./mongodbIcon.png"],
     link: "https://akracingpage-vue.onrender.com/",
@@ -41,7 +50,12 @@ const items = [
   {
     id: 4,
     title: "Podify",
-    img: "./podify.png",
+    images: [
+      "./podify1.png",
+      "./podify2.png",
+      "./podify3.png",
+      "./podify4.png",
+    ],
     desc: "Podify is a music streaming app developed with React Native and TypeScript on the frontend, and Node.js with MongoDB on the backend. The app allows users to listen to their favorite tracks, create and manage personalized playlists, and add songs to their favorites. Users can also upload their own music and share it with others, creating a community-driven platform for discovering new music. With features similar to Spotify, Podify offers a smooth, user-friendly experience, allowing you to enjoy and share music anytime, anywhere.",
     tools: [
       "./reactNativeImg.png",
@@ -54,7 +68,12 @@ const items = [
   {
     id: 5,
     title: "Ecommerce App",
-    img: "./amazonImg.png",
+    images: [
+      "./amazon1.png",
+      "./amazon2.png",
+      "./amazon3.png",
+      "./amazon4.png",
+    ],
     desc: "The E-commerce app is an online shopping platform similar to Amazon, developed with React Native on the frontend and Node.js on the backend, with MongoDB for database management. It allows users to browse and purchase a wide variety of products across different categories. The app provides a smooth and user-friendly experience, enabling users to search for items, add them to their cart, and complete transactions securely. With a vast product selection and efficient order management, the app offers a comprehensive solution for shopping online from anywhere, anytime.",
     tools: [
       "./reactNativeImg.png",
@@ -85,7 +104,23 @@ const Single = ({ item }) => {
       <div className="container">
         <div className="wrapper">
           <div className="imageContainer" ref={ref}>
-            <img src={item.img} alt="" />
+            {item.images.map((img, index) => (
+              <img src={img} alt="" key={index} />
+            ))}
+          </div>
+          <div className="tabletMobile">
+            <Swiper
+              spaceBetween={10}
+              slidesPerView={1}
+              navigation={true}
+              modules={[Navigation]}
+            >
+              {item.images.map((img, index) => (
+                <SwiperSlide key={index}>
+                  <img src={img} alt="" />
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
           <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
